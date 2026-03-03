15a27765e8317768ba5741752ffc61c504dd2982
Ikra Narin Soran <ikranarin220@gmail.com>
2026-03-03 22:47:12 +0300

    feat: added day1 variables and input example

---
+++ week1-basics/day1_variables.py
@@ -0,0 +1,10 @@
+# Day 1 - Variables and Input
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Hello", name)
+print("You are", age, "years old.")
+
+birth_year = 2026 - age
+print("You were probably born in", birth_year)
