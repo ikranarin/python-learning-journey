adf879953d81bde395b98aa3891b0b585f09c5df
Ikra Narin Soran <ikranarin220@gmail.com>
2026-03-08 17:07:18 +0300

    feat: added logical operator examples and password validation to day4

--- week1-basics/day4_login_system.py
+++ week1-basics/day4_login_system.py
@@ -3,6 +3,11 @@
 username = input("Enter username: ")
 password = input("Enter password: ")
 
+if any(char.isdigit() for char in password):
+    print("Your password is valid.")
+else:
+    print("Password must contain a number.")
+
 if len(password) < 3:
     print("Password too short")
 
@@ -14,3 +19,19 @@ elif username != "admin":
 
 elif password != "1234":
     print("Wrong password")
+
+#OR Example
+
+age = int(input("Enter your age: "))
+
+if age < 18 or age > 65:
+    print("You are in a special age group")
+else:
+    print("Standard age group")
+
+#NOT Example
+
+is_logged_in = False
+
+if not is_logged_in:
+    print("Please log in first")
